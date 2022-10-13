0d1e9634942cbf4f178f151ed960501579431120
unknown <Sulaiman>
2022-10-13 21:42:11 +0530

    main

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const route=require('./login')
 mainn.use(route)
 
 const chaat=require('./chatting')
+
 mainn.use(chaat);
 
+mainn.use('/',(req,res,next)=>{
 
+    res.status(404).send('<h1> Page not found</h1>')
+})
 console.log("hello")
 mainn.listen(5000)
+
